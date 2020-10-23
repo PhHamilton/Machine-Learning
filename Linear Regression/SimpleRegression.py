ebc7815f1886a1adba0592a98ab21da5c0845cf4
Philip Hamilton <Philip@Philips-MacBook-Pro.local>
2020-10-23 10:22:41 +0200

    Added function to print error

--- Linear Regression/SimpleRegression.py
+++ Linear Regression/SimpleRegression.py
@@ -3,34 +3,48 @@ import pandas as pd
 import pylab as pl
 import numpy as np
 from sklearn import linear_model
+from sklearn.metrics import r2_score
+
+
 
 class SimpleRegression:
     def __init__(self, data, head):
         self.data = pd.read_csv("FuelConsumption.csv")
         self.cdf = self.data[head]
-        self.msk = None
+        self.linReg = None
         self.theta = self.theta0 = None
         self.trainData = self.trainX = self.trainY = None
+        self.testData = None
         self.dataIsTrained = False
 
     def train(self, dataX, dataY, trainProcentage):
-        self.msk = self.maskData(trainProcentage)
+        msk = self.maskData(trainProcentage)
 
-        self.trainData = self.cdf[self.msk]
+        self.trainData = self.cdf[msk]
+        self.testData = self.cdf[~msk]
 
-        linReg = linear_model.LinearRegression()
+        self.linReg = linear_model.LinearRegression()
         self.trainX = np.asanyarray(self.trainData[[dataX]])
         self.trainY = np.asanyarray(self.trainData[[dataY]])
 
-        linReg.fit(self.trainX, self.trainY)
+        self.linReg.fit(self.trainX, self.trainY)
 
-        self.theta = linReg.coef_
-        self.theta0 = linReg.intercept_
+        self.theta = self.linReg.coef_
+        self.theta0 = self.linReg.intercept_
 
         self.dataIsTrained = True
 
     def maskData(self, trainProcentage):
         return np.random.rand(len(self.data)) < trainProcentage
+
+    def printError(self, dataX, dataY):
+        test_x = np.asanyarray(self.testData[[dataX]])
+        test_y = np.asanyarray(self.testData[[dataY]])
+        test_y_ = self.linReg.predict(test_x)
+
+        print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
+        print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
+        print("R2-score: %.2f" % r2_score(test_y , test_y_) )
 
     def plotTrainedData(self, dataX, dataY, graphColor = 'blue'):
         if(self.dataIsTrained is True):
@@ -42,11 +56,12 @@ class SimpleRegression:
         else:
             print("Data is not trained yet!")
 
-
+            
 
 data = pd.read_csv("FuelConsumption.csv")
 head = ['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']
 
 test = SimpleRegression(data, head)
 test.train('ENGINESIZE', 'CO2EMISSIONS', 0.8)
+test.printError('ENGINESIZE', 'CO2EMISSIONS')
 test.plotTrainedData('ENGINESIZE', 'CO2EMISSIONS')
