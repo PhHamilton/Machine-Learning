4ef91a78cbe08ffeb251335568114d924d40dea8
Philip Hamilton <Philip@Philips-MacBook-Pro.local>
2020-10-23 10:26:03 +0200

    Removed the need to input arguments in the printing and plotting function

--- Linear Regression/SimpleRegression.py
+++ Linear Regression/SimpleRegression.py
@@ -10,6 +10,7 @@ from sklearn.metrics import r2_score
 class SimpleRegression:
     def __init__(self, data, head):
         self.data = pd.read_csv("FuelConsumption.csv")
+        self.dataX = self.dataY = None
         self.cdf = self.data[head]
         self.linReg = None
         self.theta = self.theta0 = None
@@ -20,6 +21,8 @@ class SimpleRegression:
     def train(self, dataX, dataY, trainProcentage):
         msk = self.maskData(trainProcentage)
 
+        self.dataX = dataX
+        self.dataY = dataY
         self.trainData = self.cdf[msk]
         self.testData = self.cdf[~msk]
 
@@ -37,31 +40,31 @@ class SimpleRegression:
     def maskData(self, trainProcentage):
         return np.random.rand(len(self.data)) < trainProcentage
 
-    def printError(self, dataX, dataY):
-        test_x = np.asanyarray(self.testData[[dataX]])
-        test_y = np.asanyarray(self.testData[[dataY]])
+    def printError(self):
+        test_x = np.asanyarray(self.testData[[self.dataX]])
+        test_y = np.asanyarray(self.testData[[self.dataY]])
         test_y_ = self.linReg.predict(test_x)
 
         print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_ - test_y)))
         print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_ - test_y) ** 2))
         print("R2-score: %.2f" % r2_score(test_y , test_y_) )
 
-    def plotTrainedData(self, dataX, dataY, graphColor = 'blue'):
+    def plotTrainedData(self, graphColor = 'blue'):
         if(self.dataIsTrained is True):
-            plt.scatter(getattr(self.trainData, dataX), getattr(self.trainData,dataY), color =graphColor)
+            plt.scatter(getattr(self.trainData, self.dataX), getattr(self.trainData,self.dataY), color =graphColor)
             plt.plot(self.trainX, self.theta[0]*self.trainX + self.theta0[0], '-r')
-            plt.xlabel(dataX)
-            plt.ylabel(dataY)
+            plt.xlabel(self.dataX)
+            plt.ylabel(self.dataY)
             plt.show()
         else:
             print("Data is not trained yet!")
 
-            
+
 
 data = pd.read_csv("FuelConsumption.csv")
 head = ['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']
 
 test = SimpleRegression(data, head)
 test.train('ENGINESIZE', 'CO2EMISSIONS', 0.8)
-test.printError('ENGINESIZE', 'CO2EMISSIONS')
-test.plotTrainedData('ENGINESIZE', 'CO2EMISSIONS')
+test.printError()
+test.plotTrainedData()
